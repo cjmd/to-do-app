a94ee549754596c0ee983027fc6a0ae182059309
cjmd <catherinejade.deleon@gmail.com>
2020-06-10 21:58:20 -0700

    complete status and local storage.

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+toDoStorage = window.localStorage;
+
 function onReady() {
+  let id = 0;
+
   let toDos = [];
-  const addToDoForm = document.getElementById('addToDoFor');
+
+	if (toDoStorage.length != 0) {
+		var toDoString = toDoStorage.getItem('toDoKey');
+		var storedToDos = JSON.parse(toDoString);
+		toDos = storedToDos;
+	}
+
+  const addToDoForm = document.getElementById('addToDoForm');
 
   function createNewToDo() {
     const newToDoText = document.getElementById('newToDoText');
@@ -8,11 +19,13 @@ function onReady() {
 
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id
     });
 
     newToDoText.value = '';
-
+    id++;
+    toDoStorage.setItem('toDoKey',JSON.stringify(toDos));
     renderTheUI();
   }
 
@@ -20,18 +33,49 @@ function onReady() {
     const toDoList = document.getElementById('toDoList');
 
     toDoList.textContent = '';
-
+    console.log(toDos);
     toDos.forEach(function(toDo) {
-      const newLi = document.creatElement('li');
+      const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
       checkbox.type = 'checkbox';
+      checkbox.id = toDo.id;
+      checkbox.complete = toDo.complete;
+      if (checkbox.complete == true) {
+        checkbox.checked = true;
+      }
+
+      const deleteBtn = document.createElement('button');
+      deleteBtn.innerHTML = "Delete";
+      deleteBtn.id = toDo.id;
+      deleteBtn.classList.add("mdl-button");
 
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
-    });
+      newLi.appendChild(deleteBtn);
 
+      deleteBtn.addEventListener('click', event => {
+        toDos = toDos.filter(todo => todo.id != deleteBtn.id);
+        toDoStorage.setItem('toDoKey',JSON.stringify(toDos));
+        renderTheUI();
+      });
+
+      checkbox.addEventListener('click', event => {
+        if (checkbox.complete == false) {
+          checkbox.complete = true;
+        } else {
+          checkbox.complete = false;
+        }
+        toDos.forEach(todo => {
+          if (todo.id == checkbox.id) {
+            todo.complete = checkbox.complete;
+          }
+        });
+        toDoStorage.setItem('toDoKey',JSON.stringify(toDos));
+        renderTheUI();
+      });
+    });
   }
 
   addToDoForm.addEventListener('submit', event => {
